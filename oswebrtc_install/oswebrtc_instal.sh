8f97b51c12de1923b056c996c6b55d494674a50c
ManfredAabye <zainhofer@gmx.de>
2025-08-15 20:19:55 +0200

    Update oswebrtc_instal.sh

--- oswebrtc_install/oswebrtc_instal.sh
+++ oswebrtc_install/oswebrtc_instal.sh
@@ -7,6 +7,7 @@ function webRTCinstall() {
 
     # Überprüfe, ob das Verzeichnis existiert
     if [ ! -d "os-webrtc-janus" ]; then
+		git clone https://github.com/opensim/opensim.git opensim
         git clone https://github.com/Misterblue/os-webrtc-janus.git os-webrtc-janus
     else
         echo "Das Repository 'os-webrtc-janus' ist bereits vorhanden."
@@ -16,6 +17,12 @@ function webRTCinstall() {
 
     cp -r /opt/os-webrtc-janus/WebRTC-Sandbox/OpenSim /opt/opensim/OpenSim
     cp -r /opt/os-webrtc-janus/WebRTC-Sandbox/bin /opt/opensim/bin
+
+ 	echo "Running pre-build scripts..."
+	cd opensim || exit
+	bash runprebuild.sh
+	dotnet build --configuration Release OpenSim.sln
+	cd ..
 }
 
 webRTCinstall
